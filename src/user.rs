9d269fe256af39f8b7b09d7eda1d72bbcd9bbf9b
jo53289 <jo53289@student.uni-lj.si>
2026-04-23 08:45:04 +0200

    dodala funkcije in teste v user

--- src/user.rs
+++ src/user.rs
@@ -11,5 +11,39 @@ impl User {
     pub fn new(id: UserId, name: String, email: String) -> Self {
         Self { id, name, email }
     }
+
+    pub fn update_name(&mut self, new_name: String) {
+        self.name = new_name;
+    }
+
+    pub fn update_email(&mut self, new_email: String) {
+        self.email = new_email;
+    }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_user_new() {
+        let user = User::new(1, "Janez Novak".into(), "janeznovak@example.com".into());
+        assert_eq!(user.id, 1);
+        assert_eq!(user.name, "Janez Novak");
+        assert_eq!(user.email, "janeznovak@example.com");
+    }
+
+    #[test]
+    fn test_user_update_name() {
+        let mut user = User::new(1, "Janez Novak".into(), "janeznovak@example.com".into());
+        user.update_name("Janez Novak".into());
+        assert_eq!(user.name, "Janez Novak");
+    }
+
+    #[test]
+    fn test_user_update_email() {
+        let mut user = User::new(1, "Janez Novak".into(), "janeznovak@example.com".into());
+        user.update_email("janeznovak2@example.com".into());
+        assert_eq!(user.email, "janeznovak2@example.com");
+    }
+}
