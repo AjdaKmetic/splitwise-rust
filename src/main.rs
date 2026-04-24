8281fa99e2ef45fe014ab467833c70c18e2ef0f8
Ajda Kmetič <kmetic.ajda@gmail.com>
2026-04-24 23:40:53 +0200

    popravljen main.rs in dodan debt.rs

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,8 @@ fn main() {
     let Janez = User::new(1, "Janez Novak", "janeznovak@example.com");
     let Marija = User::new(2, "Marija Novak", "marijanovak@example.com");
     let mut group = Group::new(1, "Amsterdam");
-    group.add_member(Janez);
-    group.add_member(Marija);
+    group.add_member(Janez.id);
+    group.add_member(Marija.id);
 
     let expenses = vec![
         Expense::new(
@@ -18,15 +18,15 @@ fn main() {
             "Hotel".into(),
             200.0,
             Janez.id,
-            group.id,
+            Some(group.id),
             Split::Equal(vec![Janez.id, Marija.id]),
         ),
         Expense::new(
             2,
             "Dinner".into(),
             100.0,
-            &Marija,
-            group.id,
+            Marija.id,
+            Some(group.id),
             Split::Exact(vec![(Janez.id, 70.0), (Marija.id, 30.0)]),
         ),
     ];
